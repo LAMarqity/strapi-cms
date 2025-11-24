ffddd1084c9121227d2ca839294495e768421542
Linus <linus@marqity.com>
2025-11-24 16:08:44 +0100

    asd

--- src/api/live-course/controllers/live-course.js
+++ src/api/live-course/controllers/live-course.js
@@ -6,4 +6,42 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::live-course.live-course');
+module.exports = createCoreController('api::live-course.live-course', ({ strapi }) => ({
+  async find(ctx) {
+    // Add default populate for nested instructor profile_pic
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        featuredImage: true,
+        featuredVideo: true,
+        instructors: {
+          populate: {
+            profile_pic: true,
+          },
+        },
+      },
+    };
+
+    const { data, meta } = await super.find(ctx);
+    return { data, meta };
+  },
+
+  async findOne(ctx) {
+    // Add default populate for nested instructor profile_pic
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        featuredImage: true,
+        featuredVideo: true,
+        instructors: {
+          populate: {
+            profile_pic: true,
+          },
+        },
+      },
+    };
+
+    const { data, meta } = await super.findOne(ctx);
+    return { data, meta };
+  },
+}));
